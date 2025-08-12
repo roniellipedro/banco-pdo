ad5de4cbce37aaf5856c5c4d13476331584d5d8a
Roni <ronielli.acicredi@sicoob.com.br>
2025-08-12 11:25:04 -0300

    atualizando dados através do PDO

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'connection.php';
+
+$sql = 'UPDATE usuarios SET name = :name, username = :username, password = :password WHERE id = :id';
+
+$_name = 'Testada';
+$_username = 'testada';
+$_password = '789456';
+$_id = 1;
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(':name', $_name);
+$stmt->bindParam(':username', $_username);
+$stmt->bindParam(':password', $_password);
+$stmt->bindParam(':id', $_id);
+
+$queryExecute = $stmt->execute();
+
+if ($queryExecute) {
+    echo 'Usuário ' . $_name . ' atualizado!';
+} else {
+    echo 'Erro ao atualizar';
+}
