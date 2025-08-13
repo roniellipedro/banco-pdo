0f382b453d453a7bf12e328c4ada7ec7e911b0a5
Roni <ronielli.acicredi@sicoob.com.br>
2025-08-13 12:32:03 -0300

    utilizando savepoint na transaction para conseguir dar rollback em pontos especificos

--- transaction.php
+++ transaction.php
@@ -2,44 +2,94 @@
 
 require_once 'connection.php';
 
-$pdo->beginTransaction();
 
-$sql = "INSERT INTO usuarios (username,name,password) VALUES (:username,:name,:password)";
+try {
+    $pdo->beginTransaction();
 
-$username = 'Usuário 1111';
-$name = 'usuario1';
-$password = 123456;
+    $sqlAllUsers = "SELECT * FROM usuarios";
 
-$stmt = $pdo->prepare($sql);
+    $stmtAllUsers = $pdo->prepare($sqlAllUsers);
+    $result = $stmtAllUsers->execute();
 
-$stmt->bindParam(':username', $username);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':password', $password);
+    $sql = "INSERT INTO usuarios (username,name,password) VALUES (:username,:name,:password)";
 
-$stmt->execute();
+    $username1 = 'usuario1';
+    $name1 = 'Usuário 1';
+    $password1 = 123456;
 
-if ($stmt->rowCount() > 0) {
-    echo "Usuário 1 criado";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':username', $username1);
+    $stmt->bindParam(':name', $name1);
+    $stmt->bindParam(':password', $password1);
+
+    $stmt->execute();
+
+    // if ($stmt->rowCount() > 0) {
+    //     echo "Usuário 1 criado";
+    // }
+
+    $pdo->exec('SAVEPOINT user1_savepoint');
+
+    $sql = "INSERT INTO usuarios (username,name,password) VALUES (:username,:name,:password)";
+
+    $username2 = 'usuario2';
+    $name2 = 'Usuário 2';
+    $password2 = 123456;
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':username', $username2);
+    $stmt->bindParam(':name', $name2);
+    $stmt->bindParam(':password', $password2);
+
+    $stmt->execute();
+
+    // if ($stmt->rowCount() > 0) {
+    //     echo "Usuário 2 criado";
+    // }
+
+    $pdo->exec('SAVEPOINT user2_savepoint');
+
+    $sql = "INSERT INTO usuarios (username,name,password) VALUES (:username,:name,:password)";
+
+    $username3 = 'usuario3';
+    $name3 = 'Usuário 3';
+    $password3 = 123456;
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':username', $username3);
+    $stmt->bindParam(':name', $name3);
+    $stmt->bindParam(':password', $password3);
+
+    $stmt->execute();
+
+    // if ($stmt->rowCount() > 0) {
+    //     echo "Usuário 3 criado";
+    // }
+
+    $pdo->exec('SAVEPOINT user3_savepoint');
+
+    $pdo->exec('ROLLBACK TO user2_savepoint');
+
+    $pdo->commit();
+
+    // if ($stmtAllUsers) {
+    //     $data = $stmtAllUsers->fetchAll(PDO::FETCH_OBJ);
+
+    //     foreach ($data as $line) {
+    //         if ($line->username === $username1 || $line->username === $username2) {
+    //             $pdo->rollBack();
+    //         } else {
+    //             echo 'Usuários adicionados com sucesso';
+    //         }
+    //     }
+    //     $pdo->commit();
+    // }
+} catch (PDOException $e) {
+    $resultCode = $e->getCode();
+    if ($resultCode == 23000) {
+        echo 'Usuário já existe no banco de dados, desfazendo ultimas inserções.';
+    }
 }
-
-echo '<hr>';
-
-$sql = "INSERT INTO usuarios (username,name,password) VALUES (:username,:name,:password)";
-
-$username = 'Usuário 2222';
-$name = 'usuario2';
-$password = 123456;
-
-$stmt = $pdo->prepare($sql);
-
-$stmt->bindParam(':username', $username);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':password', $password);
-
-$stmt->execute();
-
-if ($stmt->rowCount() > 0) {
-    echo "Usuário 2 criado";
-}
-
-// $pdo->commit();
